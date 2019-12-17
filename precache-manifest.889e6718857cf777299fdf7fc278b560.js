c02ffcc43d57b0f17e1b44f6ba98ab760bbfa351
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-12-17 16:37:54 +0100

    Updates

---
+++ precache-manifest.889e6718857cf777299fdf7fc278b560.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "909466fb50bd2b1abc4eaf776086e873",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ea0061c9024cd354358c",
+    "url": "/monsters-rolodex/static/css/main.1c9eb7d4.chunk.css"
+  },
+  {
+    "revision": "6ef49256656a9eb2de46",
+    "url": "/monsters-rolodex/static/js/2.8629efad.chunk.js"
+  },
+  {
+    "revision": "ea0061c9024cd354358c",
+    "url": "/monsters-rolodex/static/js/main.0e6929cb.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
